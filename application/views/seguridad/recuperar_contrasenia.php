1279560369ca0b2e84b3f7f2b727a5777593a11c
Jorge Estrella <jorge.alph@gmail.com>
2018-11-01 19:05:30 -0600

    - Se eliminan datos indeseados en la plantilla (Error que mostraba un consulta sql al cargar)

--- application/views/seguridad/recuperar_contrasenia.php
+++ application/views/seguridad/recuperar_contrasenia.php
@@ -222,39 +222,6 @@
 
 	<!-- Footer Scripts
 	============================================= -->
-	
-
-  	ALTER TABLE public."Usuario" DROP COLUMN "dFechaRegistro";
-
-  	ALTER TABLE public."Usuario"
-    	ADD COLUMN "dFechaRegitro" timestamp without time zone;
-
-	ALTER TABLE public."Comentario" DROP COLUMN "dFecha";
-
-	ALTER TABLE public."Comentario"
-    	ADD COLUMN "dFecha" timestamp without time zone;
-
-	ALTER TABLE public."ComentarioLike" DROP COLUMN "dFecha";
-
-	ALTER TABLE public."ComentarioLike"
-    	ADD COLUMN "dFecha" timestamp without time zone;
-
-	ALTER TABLE public."Propuesta"
-    	ADD COLUMN "dFecha" timestamp without time zone;
-
-    ALTER TABLE public."PropuestaAdjunto" DROP COLUMN "dFecha";
-
-	ALTER TABLE public."PropuestaAdjunto"
-    	ADD COLUMN "dFecha" timestamp without time zone;
-
-	ALTER TABLE public."PropuestaLike" DROP COLUMN "dFecha";
-
-	ALTER TABLE public."PropuestaLike"
-    	ADD COLUMN "dFecha" timestamp without time zone;
-
-	ALTER TABLE public."VotoPropuesta" DROP COLUMN "dFecha";
-
-	ALTER TABLE public."VotoPropuesta"
-    	ADD COLUMN "dFecha" timestamp without time zone;
+  	
 </body>
 </html>
